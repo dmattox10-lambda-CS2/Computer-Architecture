1444e7406bd69f4c0ea43a5fe5308854b0c9ab8c
Daniel Mattox <dmattox10@gmail.com>
2020-10-12 15:55:14 -0400

    MVP Day 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,16 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.register = [0] * 8
+        self.counter = 0
+        self.halted = False
+
+        self.instruction_set = {
+            0b00000001: self.HLT,
+            0b10000010: self.LDI,
+            0b01000111: self.PRN
+        }
 
     def load(self):
         """Load a program into memory."""
@@ -35,7 +44,7 @@ class CPU:
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.register[reg_a] += self.register[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -47,19 +56,47 @@ class CPU:
         """
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
+            self.counter,
             #self.fl,
             #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
+            self.ram_read(self.counter),
+            self.ram_read(self.counter + 1),
+            self.ram_read(self.counter + 2)
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.register[i], end='')
 
         print()
 
     def run(self):
         """Run the CPU."""
-        pass
+        while not self.halted:
+            current_instruction = self.ram_read(self.counter)
+            if current_instruction in self.instruction_set:
+                self.instruction_set[current_instruction]()
+            
+            else:
+                print(f"No {current_instruction} at {self.counter}")
+                sys.exit(1)
+
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
+
+    def HLT(self):
+        self.halted = True
+
+    def LDI(self):
+        address = self.ram_read(self.counter + 1)
+        value = self.ram_read(self.counter + 2)
+        self.register[address] = value
+        self.counter += 3
+
+    def PRN(self):
+        address = self.ram_read(self.counter + 1)
+        value = self.register[address]
+        print(value)
+        self.counter += 2
